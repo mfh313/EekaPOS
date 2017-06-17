2fa38a5e817cce1af269ec73d99c7440ec4114bc
mafanghua <980992720@qq.com>
2017-06-17 22:01:46 +0800

    no message

--- EekaPOS/EekaPOS/Module/MainFrame/EPCameraScanViewController.m
+++ EekaPOS/EekaPOS/Module/MainFrame/EPCameraScanViewController.m
@@ -9,6 +9,10 @@
 #import "EPCameraScanViewController.h"
 
 @interface EPCameraScanViewController ()
+{
+    UIView *_bottomItemsView;
+    UIButton *_btnFlash;
+}
 
 @end
 
@@ -22,28 +26,54 @@
     self.style = [[self class] notSquare];
     self.isOpenInterestRect = YES;
     
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
     [self setFlashView];
+    
 }
 
 -(void)setFlashView
 {
+    if (_bottomItemsView) {
+        return;
+    }
+    
     //设置闪光灯
-    
-    UIView *bottomItemsView = [[UIView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(self.view.frame)-164,
+    _bottomItemsView = [[UIView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(self.view.frame)-164,
                                                                    CGRectGetWidth(self.view.frame), 100)];
-    bottomItemsView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.6];
+    _bottomItemsView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.6];
     
-    [self.view addSubview:bottomItemsView];
+    [self.view addSubview:_bottomItemsView];
     
     CGSize size = CGSizeMake(65, 87);
-    UIButton *btnFlash = [UIButton buttonWithType:UIButtonTypeCustom];
-    btnFlash.frame = CGRectMake(0, 0, size.width, size.height);
-    btnFlash.center = CGPointMake(CGRectGetWidth(bottomItemsView.frame)/2, CGRectGetHeight(bottomItemsView.frame)/2);
-    [btnFlash setImage:[UIImage imageNamed:@"CodeScan.bundle/qrcode_scan_btn_flash_nor"] forState:UIControlStateNormal];
-    [btnFlash addTarget:self action:@selector(openOrCloseFlash) forControlEvents:UIControlEventTouchUpInside];
+    _btnFlash = [UIButton buttonWithType:UIButtonTypeCustom];
+    _btnFlash.frame = CGRectMake(0, 0, size.width, size.height);
+    _btnFlash.center = CGPointMake(CGRectGetWidth(_bottomItemsView.frame)/2, CGRectGetHeight(_bottomItemsView.frame)/2);
+    [_btnFlash setImage:[UIImage imageNamed:@"CodeScan.bundle/qrcode_scan_btn_flash_nor"] forState:UIControlStateNormal];
+    [_btnFlash addTarget:self action:@selector(openOrCloseFlash) forControlEvents:UIControlEventTouchUpInside];
     
-    [bottomItemsView addSubview:btnFlash];
+    [_bottomItemsView addSubview:_btnFlash];
     
+}
+
+//开关闪光灯
+- (void)openOrCloseFlash
+{
+    [super openOrCloseFlash];
+    
+    if (self.isOpenFlash)
+    {
+        [_btnFlash setImage:[UIImage imageNamed:@"CodeScan.bundle/qrcode_scan_btn_flash_down"] forState:UIControlStateNormal];
+    }
+    else
+    {
+        [_btnFlash setImage:[UIImage imageNamed:@"CodeScan.bundle/qrcode_scan_btn_flash_nor"] forState:UIControlStateNormal];
+    }
 }
 
 #pragma mark -实现类继承该方法，作出对应处理
