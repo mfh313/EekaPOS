835102fbdc5f6f4d4312c43026fb31f16c7a63fc
mafanghua <980992720@qq.com>
2017-06-18 10:04:09 +0800

    no message

--- EekaPOS/EekaPOS/Module/MainFrame/SaleBilling/EPSaleBillingDiscountInputView.m
+++ EekaPOS/EekaPOS/Module/MainFrame/SaleBilling/EPSaleBillingDiscountInputView.m
@@ -8,7 +8,25 @@
 
 #import "EPSaleBillingDiscountInputView.h"
 
+@interface EPSaleBillingDiscountInputView () <UITextFieldDelegate>
+{
+    __weak IBOutlet UILabel *_discountAfterLabel;
+    __weak IBOutlet UILabel *_discountPreLabel;
+    __weak IBOutlet UILabel *_discountRateLabel;
+    __weak IBOutlet UITextField *_discountInputTextField;
+    
+}
+
+@end
+
 @implementation EPSaleBillingDiscountInputView
+
+-(void)awakeFromNib
+{
+    [super awakeFromNib];
+    _discountAfterLabel.textColor = [UIColor hx_colorWithHexString:@"0080C0"];
+    _discountRateLabel.text = @"1.0";
+}
 
 
 
