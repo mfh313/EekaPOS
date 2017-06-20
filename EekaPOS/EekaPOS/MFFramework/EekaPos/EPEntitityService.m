7cba3d70d921af3ff3e3e42c4dcb2436a205602b
mafanghua <980992720@qq.com>
2017-06-20 10:03:52 +0800

    no message

--- EekaPOS/EekaPOS/MFFramework/EekaPos/EPEntitityService.m
+++ EekaPOS/EekaPOS/MFFramework/EekaPos/EPEntitityService.m
@@ -58,5 +58,32 @@
     return m_employees;
 }
 
+/*
+ 
+ -(void)getIndividual:(NSString *)telephone
+ {
+ __weak typeof(self) weakSelf = self;
+ 
+ EPGetIndividualApi *getIndividualApi = [EPGetIndividualApi new];
+ getIndividualApi.brandId = @(1001);
+ getIndividualApi.telephone = telephone;
+ 
+ [getIndividualApi startWithCompletionBlockWithSuccess:^(YTKBaseRequest * request) {
+ 
+ if (!getIndividualApi.messageSuccess) {
+ [self showTips:getIndividualApi.errorMessage];
+ return;
+ }
+ 
+ __strong typeof(weakSelf) strongSelf = weakSelf;
+ 
+ 
+ } failure:^(YTKBaseRequest * request) {
+ NSString *errorDesc = [NSString stringWithFormat:@"错误状态码=%@\n错误原因=%@",@(request.error.code),[request.error localizedDescription]];
+ [self showTips:errorDesc];
+ }];
+ }
+ 
+ */
 
 @end
