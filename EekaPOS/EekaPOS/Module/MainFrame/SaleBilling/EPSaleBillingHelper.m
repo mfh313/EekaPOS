f69b9b4591c7580d3894121c71822cff910f68c6
mafanghua <980992720@qq.com>
2017-06-20 16:23:01 +0800

    no message

--- EekaPOS/EekaPOS/Module/MainFrame/SaleBilling/EPSaleBillingHelper.m
+++ EekaPOS/EekaPOS/Module/MainFrame/SaleBilling/EPSaleBillingHelper.m
@@ -30,7 +30,7 @@
 + (NSString *)getMonthBeginWith:(NSString *)dateStr
 {
     NSDateFormatter *format=[[NSDateFormatter alloc] init];
-    [format setDateFormat:@"yyyy-MM"];
+    [format setDateFormat:@"yyyy-MM-dd"];
     NSDate *newDate=[format dateFromString:dateStr];
     double interval = 0;
     NSDate *beginDate = nil;
@@ -38,19 +38,20 @@
     NSCalendar *calendar = [NSCalendar currentCalendar];
     
     [calendar setFirstWeekday:2];//设定周一为周首日
-    BOOL ok = [calendar rangeOfUnit:NSCalendarUnitMonth startDate:&beginDate interval:&interval forDate:newDate];
+    BOOL rangeOfUnit = [calendar rangeOfUnit:NSCalendarUnitMonth startDate:&beginDate interval:&interval forDate:newDate];
     //分别修改为 NSDayCalendarUnit NSWeekCalendarUnit NSYearCalendarUnit
-    if (ok) {
+    if (rangeOfUnit)
+    {
         endDate = [beginDate dateByAddingTimeInterval:interval-1];
-    }else {
+    }
+    else
+    {
         return @"";
     }
     
     NSDateFormatter *myDateFormatter = [[NSDateFormatter alloc] init];
     [myDateFormatter setDateFormat:@"yyyy-MM-dd"];
     NSString *beginString = [myDateFormatter stringFromDate:beginDate];
-//    NSString *endString = [myDateFormatter stringFromDate:endDate];
-//    NSString *s = [NSString stringWithFormat:@"%@-%@",beginString,endString];
     return beginString;
 }
 
