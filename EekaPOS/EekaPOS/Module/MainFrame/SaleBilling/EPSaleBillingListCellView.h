106c70f5120382e40c4d10f8e49aeab38aaf60ff
mafanghua <980992720@qq.com>
2017-06-20 14:25:20 +0800

    no message

---
+++ EekaPOS/EekaPOS/Module/MainFrame/SaleBilling/EPSaleBillingListCellView.h
@@ -0,0 +1,13 @@
+//
+//  EPSaleBillingListCellView.h
+//  EekaPOS
+//
+//  Created by EEKA on 2017/6/20.
+//  Copyright © 2017年 eeka. All rights reserved.
+//
+
+#import "MMUIBridgeView.h"
+
+@interface EPSaleBillingListCellView : MMUIBridgeView
+
+@end
---
+++ EekaPOS/EekaPOS/Module/MainFrame/SaleBilling/EPSaleBillingListCellView.m
@@ -0,0 +1,22 @@
+//
+//  EPSaleBillingListCellView.m
+//  EekaPOS
+//
+//  Created by EEKA on 2017/6/20.
+//  Copyright © 2017年 eeka. All rights reserved.
+//
+
+#import "EPSaleBillingListCellView.h"
+
+@interface EPSaleBillingListCellView ()
+{
+    
+}
+
+@end
+
+@implementation EPSaleBillingListCellView
+
+
+
+@end
