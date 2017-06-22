762dff496151ad02a85db7eaff411b9f639134ee
mafanghua <980992720@qq.com>
2017-06-22 11:05:29 +0800

    no message

--- EekaPOS/EekaPOS/Module/MainFrame/SaleBilling/EPSaleBillingMainViewController.m
+++ EekaPOS/EekaPOS/Module/MainFrame/SaleBilling/EPSaleBillingMainViewController.m
@@ -603,6 +603,7 @@
         
         [strongSelf showTips:@"销售开单成功"];
         
+        //15813818620
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [[EPAppViewControllerManager getAppViewControllerManager] pushSaleBillingListViewController];
         });
@@ -671,7 +672,7 @@
     [_receivablePriceLabel setText:[EPSaleBillingHelper moneyDescWithNumber:@([self receivablePrice])]];
 }
 
--(CGFloat)receivablePrice
+-(double)receivablePrice
 {
     return (_discountPrice - _deductionPrice);
 }
