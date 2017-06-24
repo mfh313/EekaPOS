6bbd420f3d6df8a8a524499c52e08a3b80c06b10
mafanghua <980992720@qq.com>
2017-06-24 08:48:50 +0800

    no message

---
+++ EekaPOS/EekaPOS/Module/MainFrame/SaleBilling/EPSaleBillingDetailDeductionItemView.h
@@ -0,0 +1,13 @@
+//
+//  EPSaleBillingDetailDeductionItemView.h
+//  EekaPOS
+//
+//  Created by EEKA on 2017/6/24.
+//  Copyright © 2017年 eeka. All rights reserved.
+//
+
+#import "MMUIBridgeView.h"
+
+@interface EPSaleBillingDetailDeductionItemView : MMUIBridgeView
+
+@end
---
+++ EekaPOS/EekaPOS/Module/MainFrame/SaleBilling/EPSaleBillingDetailDeductionItemView.m
@@ -0,0 +1,15 @@
+//
+//  EPSaleBillingDetailDeductionItemView.m
+//  EekaPOS
+//
+//  Created by EEKA on 2017/6/24.
+//  Copyright © 2017年 eeka. All rights reserved.
+//
+
+#import "EPSaleBillingDetailDeductionItemView.h"
+
+@implementation EPSaleBillingDetailDeductionItemView
+
+
+
+@end
