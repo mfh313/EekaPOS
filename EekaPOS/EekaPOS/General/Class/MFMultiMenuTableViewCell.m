26f9680c1b9dbfcd5b990313a3209399d2e34907
mafanghua <980992720@qq.com>
2017-06-24 19:27:18 +0800

    no message

---
+++ EekaPOS/EekaPOS/General/Class/MFMultiMenuTableViewCell.m
@@ -0,0 +1,21 @@
+//
+//  MFMultiMenuTableViewCell.m
+//  EekaPOS
+//
+//  Created by EEKA on 2017/6/24.
+//  Copyright © 2017年 eeka. All rights reserved.
+//
+
+#import "MFMultiMenuTableViewCell.h"
+
+@implementation MFMultiMenuTableViewCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
