4a56d084b7725e66439e2b2afdc0fabea377af2d
mafanghua <980992720@qq.com>
2017-06-27 11:45:44 +0800

    no message

--- EekaPOS/EekaPOS/Module/MainFrame/EPCameraScanViewController.m
+++ EekaPOS/EekaPOS/Module/MainFrame/EPCameraScanViewController.m
@@ -93,11 +93,11 @@
         return;
     }
     
-    //经测试，可以ZXing同时识别2个二维码，不能同时识别二维码和条形码
-    //    for (LBXScanResult *result in array) {
-    //
-    //        NSLog(@"scanResult:%@",result.strScanned);
-    //    }
+//    经测试，可以ZXing同时识别2个二维码，不能同时识别二维码和条形码
+    for (LBXScanResult *result in array) {
+
+        NSLog(@"scanResult:%@",result.strScanned);
+    }
     
     LBXScanResult *scanResult = array[0];
     
